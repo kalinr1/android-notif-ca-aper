a5365a6d95b4afa179b16bd047d2f3f05de98e77
kalinr1 <167093306+kalinr1@users.noreply.github.com>
2024-05-01 14:06:57 +0300

    Update main.py

--- main.py
+++ main.py
@@ -2,96 +2,30 @@ import re
 import urllib
 
 from fastapi import FastAPI, Request, Query, Form
-from pydantic import BaseModel
 from urllib.parse import unquote, parse_qs
 
 from telethon import TelegramClient
 
 app = FastAPI()
 
-
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
-
-
-@app.get("/hello/{name}")
-async def say_hello(name: str):
-    return {"message": f"Hello {name}"}
-
-
-def parse_url_encoded_data(data):
-    decoded_data = {}
-    for key, value in parse_qs(data.decode()).items():
-        decoded_data[key] = unquote(value[0])
-    return decoded_data
-
-
-@app.get('/test1')
-async def receive_notification(data: bytes = Form(...)):
-    form_data = parse_url_encoded_data(data)
-    name = form_data.get('name')
-    title = form_data.get('title')
-    text = form_data.get('text')
-
-    print("Name:", name)
-    print("Title:", title)
-    print("Text:", text)
-
-    return 'Received notification successfully!'
-
-
-@app.post("/tg_notif")
-async def tg_notif(request: Request):
-    request_header = request.headers
-    url = request.url
-    request_body = await request.body()
-    request_body_str = request_body.decode("utf-8")
-    print("Received request:")
-    print("Headers:", request_header)
-    print("url:", url)
-    print("Request body:", request_body)
-
-    return {"message": "Notification received"}
-
-
-@app.get("/test2")
-async def tg_notif(name: str,
-                   pkg: str,
-                   title: str,
-                   text: str,
-                   subtext: str,
-                   bigtext: str,
-                   infotext: str):
-    # Do something with the extracted data
-    print("Name:", name)
-    print("Package:", pkg)
-    print("Title:", title)
-    print("Text:", text)
-    print("Subtext:", subtext)
-    print("Bigtext:", bigtext)
-    print("Infotext:", infotext)
-
-    return {"message": "Notification received"}
-
-
-@app.post("/test2")
-async def parse_request(request: Request):
+@app.post("/telegram_notifications")
+async def handle_telegram_notifications_from_android(request: Request):
     request_body = await request.body()
     request_body = request_body.decode('utf-8')
     parsed_body = parse_qs(request_body)
 
-    name = parsed_body['name'][0]
-    pkg = parsed_body['pkg'][0]
-    title = unquote(parsed_body['title'][0])
-    text = unquote(parsed_body['text'][0])
+    chat_and_sender = unquote(parsed_body['title'][0])
+    message = unquote(parsed_body['text'][0])
 
-    print(title + " " + text)
+    print(chat_and_sender + " " + message)
+
+    scan_post_for_ca(message)
 
 
-def scan_post_for_ca(post_content: str):
+
+def scan_post_for_ca(message: str):
     ca_pattern = r'\b[a-zA-Z0-9]{32,44}\b'
-    ca_matches = re.finditer(ca_pattern, post_content)
+    ca_matches = re.finditer(ca_pattern, message)
 
     for match in ca_matches:
         ca_value = match.group()
@@ -109,3 +43,58 @@ def scan_post_for_ca(post_content: str):
 # async def trojan_ape(ca_value):
 #     await tg_client.send_message("@handle", ca_value)
 #     print("tg message")
+
+
+# def parse_url_encoded_data(data):
+#     decoded_data = {}
+#     for key, value in parse_qs(data.decode()).items():
+#         decoded_data[key] = unquote(value[0])
+#     return decoded_data
+#
+#
+# @app.get('/test1')
+# async def receive_notification(data: bytes = Form(...)):
+#     form_data = parse_url_encoded_data(data)
+#     name = form_data.get('name')
+#     title = form_data.get('title')
+#     text = form_data.get('text')
+#
+#     print("Name:", name)
+#     print("Title:", title)
+#     print("Text:", text)
+#
+#     return 'Received notification successfully!'
+#
+#
+# @app.post("/tg_notif")
+# async def tg_notif(request: Request):
+#     request_header = request.headers
+#     url = request.url
+#     request_body = await request.body()
+#     request_body_str = request_body.decode("utf-8")
+#     print("Received request:")
+#     print("Headers:", request_header)
+#     print("url:", url)
+#     print("Request body:", request_body)
+#
+#     return {"message": "Notification received"}
+#
+#
+# @app.get("/test2")
+# async def tg_notif(name: str = Query(...),
+#                    pkg: str = Query(...),
+#                    title: str = Query(...),
+#                    text: str = Query(...),
+#                    subtext: str = Query(...),
+#                    bigtext: str = Query(...),
+#                    infotext: str = Query(...)):
+#     # Do something with the extracted data
+#     print("Name:", name)
+#     print("Package:", pkg)
+#     print("Title:", title)
+#     print("Text:", text)
+#     print("Subtext:", subtext)
+#     print("Bigtext:", bigtext)
+#     print("Infotext:", infotext)
+#
+#     return {"message": "Notification received"}
